b8a4b4f46069f6bc1beabbb54e255b37949e9a1a
Alexander <filalexant@yandex.ru>
2021-02-17 00:10:15 +0300

    ok

--- Lab_6.c
+++ Lab_6.c
@@ -16,49 +16,6 @@ void init (list_t *l) {
 	l->size = 0;
 }
 
-int strcomp (char *s1, char *s2) {
-	while ((*s1 != 0) && (*s1 == *s2)) {
-		s1++;
-		s2++;
-	}
-	return *s1 - *s2;
-}
-
-void swap (char *s1, char *s2) {
-	char *tmp = s2;
-	*s2 = *s1;
-	s1 = tmp;
-}
-
-void push_back (list_t *l, char* word) {
-	node_t *n, *cur;
-	int i = 0;
-	n = (node_t*) malloc(sizeof(node_t));
-	while (word[i] != 0) {
-		n->value[i] = word[i];
-		i++;
-	}
-	n->next = NULL;
-	if (l->head == NULL) {
-		l->head = n;
-	} else {
-		cur = l->head;
-		while (cur->next != NULL) {
-			cur = cur->next;
-		}
-		cur->next = n;
-	}
-	l->size++;
-}
-
-void print (list_t *l) {
-	node_t *cur = l->head;
-	while (cur != NULL) {
-		printf("%s\n", cur->value);
-		cur = cur->next;
-	}
-}
-
 int is_letter (char str) {
 	if (((str >= 'A') && (str <= 'Z')) || ((str >= 'a') && (str <= 'z'))) {
 		return 1;
@@ -67,12 +24,66 @@ int is_letter (char str) {
 	}
 }
 
+int strcomp (char *s1, char *s2) {
+	while ((*s1 != 0) && (*s1 == *s2)) {
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
+}
+
+void swap (char **s1, char **s2) {
+	char *tmp = *s1;
+	*s1 = *s2;
+	*s2 = tmp;
+}
+
+void bubble_sort (list_t *l) {
+	node_t *cur = l->head;
+	node_t *tmp = NULL;
+	char trigger = 1;
+	while (cur || trigger) {
+		trigger = 0;
+		tmp = cur->next;
+		while (tmp) {
+			if (strcomp(cur->value, tmp->value) > 0) {
+				swap(cur->value, tmp->value);
+				trigger = 1;
+			}
+			tmp = tmp->next;
+		}
+		cur = cur->next;
+	}
+}
+
+void push_front (list_t *l, char *word) {
+	node_t *n;
+	int i = 0;
+	n = (node_t*) malloc(sizeof(node_t));
+	while (word[i] != 0) {
+		n->value[i] = word[i];
+		i++;
+	}
+	n->next = l->head;
+	l->head = n;
+	l->size++;
+}
+
+void print (list_t *l) {
+	node_t *cur = l->head;
+	while (cur) {
+		printf("%s\n", cur->value);
+		cur = cur->next;
+	}
+}
+
 int main () {
-	char a = 0;
-	int i = 0, in_word = 0, word_count = 0;
+	char a = 0, in_word = 0;
+	int i = 0;
 	list_t l1, l2;
 	init(&l1);
-	char *word = calloc(128, sizeof(char));
+	init(&l2);
+	char *word = malloc(128 * sizeof(char));
 	while ((a = getchar()) != '\n') {
 		if (is_letter(a)) {
 			word[i] = a;
@@ -80,9 +91,9 @@ int main () {
 			i++;
 		} else {
 			if (in_word) {
-				push_back(&l1, word);
-				word = calloc(128, sizeof(char));
-				word_count++;
+				push_front(&l1, word);
+				free(word);
+				word = malloc(128 * sizeof(char));
 				i = 0;
 				in_word = 0;
 			}
@@ -90,21 +101,14 @@ int main () {
 	}
 	if (in_word) {
 		word[i] = 0;
-		push_back(&l1, word);
-		word_count++;
+		push_front(&l1, word);
 	}
-	/*i = 0;
-	char *min = malloc(128 * sizeof(char));
+	bubble_sort(&l1);
 	node_t *cur = l1.head;
-	while (i < l1.size) {
-		min = cur->value;
-		while (cur != NULL) {
-			if (cur->next->value < min) {
-				min = cur->next->value;
-			}
-		}
-		i++;
-	}*/
-	print(&l1);
+	while (cur) {
+		push_front(&l2, cur->value);
+		cur = cur->next;
+	}
+	print(&l2);
 	return 0;
 }
